f94ac49e67a9548589293781e313415d76c2aff4
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-01-18 15:49:04 +0200

    adjusted urls, creted view index

--- service/views.py
+++ service/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from service.models import (Ingredient,
+                            Dishtype,
+                            Dish,
+                            Cook)
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    ingredient = Ingredient.objects.count()
+    dish_type = Dishtype.objects.count()
+    dish = Dish.objects.count()
+    cooks = Cook.objects.count()
+    context = {
+        "ingredient": ingredient,
+        "dish_type": dish_type,
+        "dish": dish,
+        "cooks": cooks
+    }
+    return render(request, "service/index.html", context=context)
