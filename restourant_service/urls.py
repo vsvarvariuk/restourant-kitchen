2baab9192727ef11a4fe354a545c06e4873ef9c6
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-02-06 12:45:48 +0200

    config settings

--- restourant_service/urls.py
+++ restourant_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for restaurant_service project.
+URL configuration for restourant_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
